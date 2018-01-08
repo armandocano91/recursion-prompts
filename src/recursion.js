2473713e88d8fcdeeae4e51303c29001cd96bef7
armandocano91 <armandocano.ac@gmail.com>
2018-01-08 14:41:27 -0600

    Fixed Problem 3

--- src/recursion.js
+++ src/recursion.js
@@ -33,10 +33,18 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (Array.isArray(array[0])){
+    return arraySum(array[0]) + arraySum(array.slice(1));
+  }
+   return array[0] + arraySum(array.slice(1));
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  //if (isEven )
 };
 
 // 5. Sum all integers below a given integer.
